78fecd0be559a0fe76110abf15ecbd668117b59a
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2025-12-28 23:46:42 -0300

    fix:automatizando migrations com knexJs

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,4 +2,12 @@ const config = require('../knexfile');
 
 const knex = require('knex')(config);
 
+knex.migrate.latest([config])
+    .then(() => {
+        console.log('Banco de dados migrado');
+    })
+    .catch((error) => {
+        console.error('Erro ao migrar o banco de dados:', error);
+    });
+
 module.exports = knex;
