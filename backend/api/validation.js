2bc2da93bf8cecd7f2e4810f2f6bda787cbd39e6
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2025-12-28 23:45:30 -0300

    fix:adicionando validatanions

---
+++ backend/api/validation.js
@@ -0,0 +1,23 @@
+// No arquivo validation.js
+module.exports = app => { // Altere de 'export default' para 'module.exports'
+    function existsOrError(value, msg) {
+        if (!value) throw msg;
+        if (Array.isArray(value) && value.length === 0) throw msg;
+        if (typeof value === 'string' && !value.trim()) throw msg;
+    }
+
+    function notExistsOrError(value, msg) {
+        try {
+            existsOrError(value, msg);
+        } catch (msg) {
+            return;
+        }
+        throw msg;
+    }
+
+    function equalsOrError(valueA, valueB, msg) {
+        if (valueA !== valueB) throw msg;
+    }
+
+    return { existsOrError, notExistsOrError, equalsOrError };
+};
