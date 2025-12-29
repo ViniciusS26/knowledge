eecffd4e66f0edb2f7f299b063142ff2b78632ff
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2025-12-29 00:29:46 -0300

    fix:adiconando rotas

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,5 +1,19 @@
 module.exports = app => {
     /* Rota para retornar informações do usuário */
-    app.route('/users').post(app.api.user.saveUser);
+    app.route('/users')
+        .post(app.api.user.saveUser)
+        .get(app.api.user.getUsers)
 
+    
+    app.route('/users/:id')
+        .put(app.api.user.saveUser)
+        .get(app.api.user.getUserById);
+    
+    app.route('/categories')
+        .post(app.api.category.saveCategory)
+        .get(app.api.category.getCategories);
+
+    app.route('/categories/:id')
+        .put(app.api.category.saveCategory)
+        .get(app.api.category.getCategoryById);
 }
